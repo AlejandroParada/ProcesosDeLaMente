56a8acf0203da416a636cf220d625ff05c458130
Alejandro Parada <45214833+AlejandroParada@users.noreply.github.com>
2020-02-21 10:42:32 -0300

    Update config.js

--- pags/.vuepress/config.js
+++ pags/.vuepress/config.js
@@ -3,13 +3,16 @@ module.exports = {
     dest: 'dist',
     base: '/ProcesosDelaMente/',
     title: 'Procesos de la Mente',
-    description: 'Procesos de la mente',
+    description: 'Los Procesos de la mente explicados de una manera sencilla',
     themeConfig: {
         sidebar: [
           'La Caja Negra',
           'Tipo Cuadrada',
           'Tipo Redonda',
-          'DatosUruguay'
+          'Tipos de entrada',
+          'Puerta de Hoja',
+          'Puerta de ascensor',
+          'Puesta de vaiben'  
         ]
     }
   }
